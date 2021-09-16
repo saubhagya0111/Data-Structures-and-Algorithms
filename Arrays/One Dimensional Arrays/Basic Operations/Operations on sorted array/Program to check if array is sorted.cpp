f3dc0f4fb0c6043e7c8e3ee0b97f3bef97f40ff7
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-09-16 12:39:58 +0530

    Create Program to check if array is sorted.cpp

---
+++ Arrays/One Dimensional Arrays/Basic Operations/Operations on sorted array/Program to check if array is sorted.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+bool is_sorted(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array\n";
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements of the array\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    if (is_sorted(a, n) == 0)
+    {
+        cout << "Array is not sorted\n";
+    }
+    else if (is_sorted(a, n) == 1)
+    {
+        cout << "Array is sorted\n";
+    }
+
+    return 0;
+}
